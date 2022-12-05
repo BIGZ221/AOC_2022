7cf3bab0e0d2669d2179fad12ef52f1d57f66190
Zach Fuchs <zachef221@gmail.com>
2022-12-04 22:39:10 -0700

    Day 5

---
+++ Day5/day5_part2.py
@@ -0,0 +1,20 @@
+import re
+
+stacks = ["T,V,J,W,N,R,M,S", "V,C,P,Q,J,D,W,B", "P,R,D,H,F,J,B", "D,N,M,B,P,R,F", "B,T,P,R,V,H", "T,P,B,C", "L,P,R,J,B", "W,B,Z,T,L,S,C,N", "G,S,L"]
+
+stacks = list(map(lambda stack: stack.split(","), stacks))
+
+with open("input.txt") as f:
+    data = f.read().splitlines(keepends=False)
+
+def make_move(count, from_index, to_index):
+    moved_stacks = stacks[from_index][:count]
+    del stacks[from_index][:count]
+    stacks[to_index] = moved_stacks + stacks[to_index]
+
+moves = [list(map(int,re.findall("\d+", move))) for move in data]
+
+for count, from_index, to_index in moves:
+    make_move(count, from_index - 1, to_index -1)
+
+print("".join([stack[0] for stack in stacks]))
