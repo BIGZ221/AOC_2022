52c3bce7c674728fc9921377dc5755fcc75b467d
Zach Fuchs <zachef221@gmail.com>
2022-12-11 23:45:32 -0700

    Day 12

---
+++ Day12/day12_part2.py
@@ -0,0 +1,68 @@
+
+import sys
+
+
+def pretty_print(matrix):
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix]))
+
+with open("input.txt") as f:
+    data = f.read().splitlines(keepends=False)
+
+height_map = list(map(lambda x: list(x), data))
+
+def find_all(height_map, val):
+    occurances = []
+    for i, row in enumerate(height_map):
+        for j, col in enumerate(row):
+            if col == val:
+                occurances.append((i, j))
+    return occurances
+
+def get_adjacent(height_map, visited, pos):
+    adj = []
+    row, col = pos
+    elevation = ord(height_map[row][col])
+    if row > 0:
+        newRow = row - 1
+        if not visited[newRow][col] and elevation - ord(height_map[newRow][col]) >= -1:
+            visited[newRow][col] = True
+            adj.append((pos, (newRow, col)))
+    if row < len(height_map) - 1:
+        newRow = row + 1
+        if not visited[newRow][col] and elevation - ord(height_map[newRow][col]) >= -1:
+            visited[newRow][col] = True
+            adj.append((pos, (newRow, col)))
+    if col > 0:
+        newCol = col - 1
+        if not visited[row][newCol] and elevation - ord(height_map[row][newCol]) >= -1:
+            visited[row][newCol] = True
+            adj.append((pos, (row, newCol)))
+    if col < len(height_map[row]) - 1:
+        newCol = col + 1
+        if not visited[row][newCol] and elevation - ord(height_map[row][newCol]) >= -1:
+            visited[row][newCol] = True
+            adj.append((pos, (row, newCol)))
+    return adj
+
+def path_to(height_map, start, target):
+    visited = [[False for _ in row] for row in height_map]
+    distances = [[0 for _ in row] for row in height_map]
+
+    neighbors = get_adjacent(height_map, visited, start)
+
+    while len(neighbors) > 0:
+        prev, point = neighbors.pop(0)
+        visited[point[0]][point[1]] = True
+        neighbors.extend(get_adjacent(height_map, visited, point))
+        distances[point[0]][point[1]] = distances[prev[0]][prev[1]] + 1
+    return distances[target[0]][target[1]]
+    
+start = find_all(height_map, "S")[0]
+target = find_all(height_map, "E")[0]
+
+height_map[start[0]][start[1]] = "a"
+height_map[target[0]][target[1]] = "z"
+
+possible_starts = find_all(height_map, "a")
+
+print(min(filter(lambda v: v > 0, [path_to(height_map, start, target) for start in possible_starts])))
